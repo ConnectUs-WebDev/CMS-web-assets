e9f6d9644600f0f965569525e95705b53b3d26a9
pta-portal-dev <webdev.connectus@gmail.com>
2025-05-17 05:31:18 +0800

    Create form-checkbox-styler.js

---
+++ form-checkbox-styler.js
@@ -0,0 +1,23 @@
+function underline(phrases) {
+	for(let id of checkboxes) {
+    	let chkbox = document.getElementById(id);
+        let label = chkbox.getElementsByTagName("label")[1];
+        let replaced = label.innerHTML;
+    	for(const phrase of phrases) {
+          replaced = replaced.replaceAll(phrase, "<span style='text-decoration: underline'>"+phrase+"</span>");
+        }
+        label.innerHTML = replaced;
+    }
+}
+
+function bold(phrases) {
+	for(let id of checkboxes) {
+    	let chkbox = document.getElementById(id);
+        let label = chkbox.getElementsByTagName("label")[1];
+        let replaced = label.innerHTML;
+    	for(const phrase of phrases) {
+          replaced = replaced.replaceAll(phrase, "<span style='font-weight: 700'>"+phrase+"</span>");
+        }
+        label.innerHTML = replaced;
+    }
+}
