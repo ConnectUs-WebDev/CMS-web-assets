1dd85533b3ad6d13dae48c190d32cadfb9df4c38
pta-portal-dev <webdev.connectus@gmail.com>
2025-05-30 04:22:02 +0800

    Update products.js

--- products.js
+++ products.js
@@ -11,4 +11,4 @@
     mnth_prods = mnth_prods.concat(bookkeeping[0], brand[0]);
     qrt_prods = qrt_prods.concat(bookkeeping[1], brand[1]);
     
-    const socmed_accs = ["Social Media & Email Marketing - Facebook Business Page", "Social Media & Email Marketing - Instagram Business Page", "Social Media & Email Marketing - LinkedIn Business Page"];
+    const socmed_accs = [/*"Social Media & Email Marketing - Facebook Business Page", "Social Media & Email Marketing - Instagram Business Page", "Social Media & Email Marketing - LinkedIn Business Page"*/];
