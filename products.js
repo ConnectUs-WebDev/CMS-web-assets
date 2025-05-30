2dd2f91c9bf734daaf3c5357d2063485bd448b55
pta-portal-dev <webdev.connectus@gmail.com>
2025-05-31 00:22:44 +0800

    Update products.js

--- products.js
+++ products.js
@@ -8,7 +8,7 @@
     const brand = [["Social Media & Email Marketing - Brand Builder", "Social Media & Email Marketing - Brand Accelerator"], ["Social Media & Email Marketing - Brand Accelerator Quarterly", "Social Media & Email Marketing - Brand Builder Quarterly"]];
     const bk_setup = ["Bookkeeping - Basic Setup", "Bookkeeping - Standard Setup", "Bookkeeping - Advanced Setup"];
     
-    mnth_prods = mnth_prods.concat(bookkeeping[0], brand[0]);
-    qrt_prods = qrt_prods.concat(bookkeeping[1], brand[1]);
+    mnth_prods = mnth_prods.concat(virtual_assistant[0], bookkeeping[0], brand[0]);
+    qrt_prods = qrt_prods.concat(virtual_assistant[1 ], bookkeeping[1], brand[1]);
     
     const socmed_accs = [/*"Social Media & Email Marketing - Facebook Business Page", "Social Media & Email Marketing - Instagram Business Page", "Social Media & Email Marketing - LinkedIn Business Page"*/];
